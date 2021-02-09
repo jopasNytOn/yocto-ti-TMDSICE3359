3981568177c4cd97fb8bf50dbac03a0681c25daf
jopasNytOn <jopasnyton@saunalahti.fi>
2021-02-09 20:49:30 +0200

    Move conf/ out of tisdk

--- Jenkinsfile
+++ Jenkinsfile
@@ -19,9 +19,9 @@ if [ -d "bin/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf" ]; then echo "gcc-a
 if [ -d "bin/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu" ]; then echo "gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu already downloaded"; else wget https://developer.arm.com/-/media/Files/downloads/gnu-a/8.3-2019.03/binrel/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu.tar.xz; tar -Jxvf gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu.tar.xz -C bin; rm gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu.tar.xz; fi
 export TOOLCHAIN_PATH_ARMV7=$PWD/bin/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf
 export TOOLCHAIN_PATH_ARMV8=$PWD/bin/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu
-if [ -d "tisdk" ]; then echo "tisdk already cloned"; else git clone git://arago-project.org/git/projects/oe-layersetup.git tisdk; fi
+if [ -d "tisdk" ]; then git -C tisdk checkout master; git -C tisdk pull; else git clone git://arago-project.org/git/projects/oe-layersetup.git tisdk; fi && git -C tisdk rev-parse --short HEAD
 cd tisdk && ./oe-layertool-setup.sh -f configs/processor-sdk/processor-sdk-06.03.00.106-config.txt
-cd build && . conf/setenv
+cd .. && mkdir -p am335x-evm/build && cd am335x-evm/build && . ../conf/setenv
 MACHINE=am335x-evm bitbake arago-base-tisdk-image
 cd ../.. && if [ -d "artifacts" ]; then rm -rf artifacts; fi && mkdir -p artifacts
 cp tisdk/build/arago-tmp-external-arm-toolchain/deploy/images/am335x-evm/* artifacts
--- build_yocto.sh
+++ build_yocto.sh
@@ -1,29 +1,17 @@
+#!/bin/bash
+
 sudo apt-get install -y git build-essential python diffstat texinfo gawk chrpath dos2unix wget unzip socat doxygen libc6:i386 libncurses5:i386 libstdc++6:i386 libz1:i386
-sudo dpkg-reconfigure dash
+if debconf-show dash | grep -q 'dash/sh: false'; then echo "dash already in use"; else sudo dpkg-reconfigure dash; fi
 
 mkdir -p bin
-if [ -d "bin/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf" ];
-then
-    echo "gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf already downloaded"
-else
-    wget https://developer.arm.com/-/media/Files/downloads/gnu-a/8.3-2019.03/binrel/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf.tar.xz
-    tar -Jxvf gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf.tar.xz -C bin
-    rm gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf.tar.xz
-fi
-if [ -d "bin/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu" ];
-then
-    echo "gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu already downloaded"
-else
-    wget https://developer.arm.com/-/media/Files/downloads/gnu-a/8.3-2019.03/binrel/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu.tar.xz
-    tar -Jxvf gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu.tar.xz -C bin
-    rm gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu.tar.xz
-fi
+if [ -d "bin/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf" ]; then echo "gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf already downloaded"; else wget https://developer.arm.com/-/media/Files/downloads/gnu-a/8.3-2019.03/binrel/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf.tar.xz; tar -Jxvf gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf.tar.xz -C bin; rm gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf.tar.xz; fi
+if [ -d "bin/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu" ]; then echo "gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu already downloaded"; else wget https://developer.arm.com/-/media/Files/downloads/gnu-a/8.3-2019.03/binrel/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu.tar.xz; tar -Jxvf gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu.tar.xz -C bin; rm gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu.tar.xz; fi
 export TOOLCHAIN_PATH_ARMV7=$PWD/bin/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf
 export TOOLCHAIN_PATH_ARMV8=$PWD/bin/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu
 
-git clone git://arago-project.org/git/projects/oe-layersetup.git tisdk
+if [ -d "tisdk" ]; then git -C tisdk checkout master; git -C tisdk pull; else git clone git://arago-project.org/git/projects/oe-layersetup.git tisdk; fi && git -C tisdk rev-parse --short HEAD
 cd tisdk && ./oe-layertool-setup.sh -f configs/processor-sdk/processor-sdk-06.03.00.106-config.txt
-cd build && . conf/setenv
+cd .. && mkdir -p am335x-evm/build && cd am335x-evm/build && . ../conf/setenv
 
 MACHINE=am335x-evm bitbake arago-base-tisdk-image
 
