d32930d31d41d67ccdb3f15668095eaf9bf4a661
Jani Soderstrom <jani.soderstrom@efore.com>
2021-03-23 12:38:38 +0200

    Enable Dunfell build

--- build_yocto.sh
+++ build_yocto.sh
@@ -2,28 +2,30 @@ sudo apt-get install -y git build-essential python diffstat texinfo gawk chrpath
 sudo dpkg-reconfigure dash
 
 mkdir -p bin
-if [ -d "bin/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf" ];
+if [ -d "bin/gcc-arm-9.2-2019.12-x86_64-arm-none-linux-gnueabihf" ];
 then
-    echo "gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf already downloaded"
+    echo "gcc-arm-9.2-2019.12-x86_64-arm-none-linux-gnueabihf already downloaded"
 else
-    wget https://developer.arm.com/-/media/Files/downloads/gnu-a/8.3-2019.03/binrel/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf.tar.xz
-    tar -Jxvf gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf.tar.xz -C bin
-    rm gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf.tar.xz
+    wget https://developer.arm.com/-/media/Files/downloads/gnu-a/9.2-2019.12/binrel/gcc-arm-9.2-2019.12-x86_64-arm-none-linux-gnueabihf.tar.xz
+    tar -Jxvf gcc-arm-9.2-2019.12-x86_64-arm-none-linux-gnueabihf.tar.xz -C bin
+    rm gcc-arm-9.2-2019.12-x86_64-arm-none-linux-gnueabihf.tar.xz
 fi
-if [ -d "bin/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu" ];
+if [ -d "bin/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu" ];
 then
-    echo "gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu already downloaded"
+    echo "gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu already downloaded"
 else
-    wget https://developer.arm.com/-/media/Files/downloads/gnu-a/8.3-2019.03/binrel/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu.tar.xz
-    tar -Jxvf gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu.tar.xz -C bin
-    rm gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu.tar.xz
+    wget https://developer.arm.com/-/media/Files/downloads/gnu-a/9.2-2019.12/binrel/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu.tar.xz
+    tar -Jxvf gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu.tar.xz -C bin
+    rm gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu.tar.xz
 fi
-export TOOLCHAIN_PATH_ARMV7=$PWD/bin/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf
-export TOOLCHAIN_PATH_ARMV8=$PWD/bin/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu
+export TOOLCHAIN_PATH_ARMV7=$PWD/bin/gcc-arm-9.2-2019.12-x86_64-arm-none-linux-gnueabihf
+export TOOLCHAIN_PATH_ARMV8=$PWD/bin/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu
 
 git clone git://arago-project.org/git/projects/oe-layersetup.git tisdk
-cd tisdk && ./oe-layertool-setup.sh -f configs/processor-sdk/processor-sdk-06.03.00.106-config.txt
+cd tisdk && ./oe-layertool-setup.sh -f configs/processor-sdk/processor-sdk-dunfell-07.01.00.txt
 cd build && . conf/setenv
 
-MACHINE=am335x-evm bitbake arago-base-tisdk-image
+cp ../../am335x-evm/sources/neo-ai-dlr.bb ../sources/meta-aws/recipes-neo-ai/neo-ai-dlr/
+
+MACHINE=am335x-evm bitbake tisdk-base-image
 
